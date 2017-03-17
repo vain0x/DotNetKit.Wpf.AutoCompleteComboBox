441f057b03395846942d92291005f7146b4e731a
vain0 <id_vain@yahoo.co.jp>
2017-03-17 11:10:21 +0900

    Fix English

--- DotNetKit.Wpf.AutoCompleteComboBox/Windows/Controls/AutoCompleteComboBoxSetting.cs
+++ DotNetKit.Wpf.AutoCompleteComboBox/Windows/Controls/AutoCompleteComboBoxSetting.cs
@@ -12,16 +12,16 @@ namespace DotNetKit.Windows.Controls
     public abstract class AutoCompleteComboBoxSetting
     {
         /// <summary>
-        /// Gets a filter which determines whether items should be suggested or not
+        /// Gets a filter function which determines whether items should be suggested or not
         /// for the specified query.
-        /// Default: The filter function maps an item to <c>true</c>
+        /// Default: Gets the filter which maps an item to <c>true</c>
         /// if its text contains the query (case insensitive, ignoring surrounding spaces).
         /// </summary>
         /// <param name="query">
-        /// The string the user input.
+        /// The string input by user.
         /// </param>
         /// <param name="stringFromItem">
-        /// The function to gets a string with which identifies the specified item.
+        /// The function to get a string which identifies the specified item.
         /// </param>
         /// <returns></returns>
         public abstract Func<object, bool>
@@ -29,8 +29,8 @@ namespace DotNetKit.Windows.Controls
 
         /// <summary>
         /// Gets an integer.
-        /// As long as sthe number of suggested items is less than the value,
-        /// this opens the drop down.
+        /// The combobox opens the drop down
+        /// if the number of suggested items is less than the value.
         /// Note that the value is larger, it's heavier to open the drop down.
         /// Default: 100.
         /// </summary>
