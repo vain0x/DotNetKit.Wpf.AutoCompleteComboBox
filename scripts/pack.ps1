5a3ed9ee2131eab40e2271a7e7ddc80db2dce822
vain0x <vainzerox@gmail.com>
2021-10-17 11:02:07 +0900

    chore: Add cheetsheet for deployment for me

--- scripts/pack.ps1
+++ scripts/pack.ps1
@@ -1,4 +1,13 @@
 #!/bin/pwsh
 # USAGE: ./scripts/pack
 
+# Cheatsheet for deployment:
+#   - bump up version number in .csproj
+#   - write CHANGELOG
+#   - commit
+#   - create Git tag
+#   - build and pack
+#   - publish to NuGet
+#   - write release note there (copy from CHANGELOG)
+
 dotnet pack DotNetKit.Wpf.AutoCompleteComboBox -c Release
