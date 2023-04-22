b5be754f058a46cb63cb51c1cbd7a29b569a89d4
vain0x <vainzerox@gmail.com>
2023-04-22 16:30:53 +0900

    Add Test project

---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainViewModel();
+        }
+
+        //public CollectionViewSource Items = new CollectionViewSource();
+    }
+}
---
+++ Test/Util/Command.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Test.Util
+{
+    sealed class Command : ICommand
+    {
+        readonly Action<object?> execute;
+
+        public Command(Action<object?> execute)
+        {
+            this.execute = execute;
+        }
+
+        public event EventHandler? CanExecuteChanged { add { } remove { } }
+        public bool CanExecute(object? _parameter) => true;
+        public void Execute(object? parameter) => execute(parameter);
+    }
+}
