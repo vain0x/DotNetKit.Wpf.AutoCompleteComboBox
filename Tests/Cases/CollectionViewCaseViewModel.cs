8e2497b25b397eff8ce9ff495260784f8a6a0779
vain0x <vainzerox@gmail.com>
2025-10-13 21:45:51 +0900

    Update tests

--- Tests/Cases/CollectionViewCaseViewModel.cs
+++ Tests/Cases/CollectionViewCaseViewModel.cs
@@ -8,34 +8,24 @@ using Tests.Util;
 
 namespace Tests.Cases
 {
-    class CollectionViewSourceCaseViewModel : ViewModelBase
+    class CollectionViewCaseViewModel : ViewModelBase
     {
-        public CollectionViewSourceCaseViewModel()
+        public CollectionViewCaseViewModel()
         {
-            items = new(PersonModule.All);
-            cvs = new CollectionViewSource
-            {
-                Source = items,
-            };
+            collectionView = new(Items);
             ReloadCommand = new Command(_ => Reload());
             ClearCommand = new Command(_ => Clear());
-
-            cvs.Filter += (_, e) =>
-            {
-                var item = (Person)e.Item;
-                e.Accepted = item.Name.Contains(Filter, StringComparison.OrdinalIgnoreCase);
-            };
         }
 
-        ObservableCollection<Person> items;
+        ObservableCollection<Person> items = new(PersonModule.All);
         public ObservableCollection<Person> Items
         {
             get => items;
             set { SetField(ref items, value); }
         }
 
-        CollectionViewSource cvs;
-        public CollectionViewSource CollectionViewSource => cvs;
+        ListCollectionView collectionView;
+        public ListCollectionView CollectionView => collectionView;
 
         Person? selectedItem;
         public Person? SelectedItem
@@ -58,33 +48,38 @@ namespace Tests.Cases
             set
             {
                 SetField(ref filter, value);
-                cvs.View.Refresh();
+
+                // Update filter.
+                collectionView.Filter = obj =>
+                {
+                    var item = (Person)obj;
+                    return item.Name.Contains(Filter, StringComparison.OrdinalIgnoreCase);
+                };
+                collectionView.Refresh();
             }
         }
 
         public ICommand ReloadCommand { get; init; }
         public void Reload()
         {
-            var newItems = new List<Person>(PersonModule.All);
+            var newItems = new ObservableCollection<Person>(PersonModule.All);
             newItems.Insert(0, GenerateRandomPerson());
-            using (cvs.DeferRefresh())
+
+            Items.Clear();
+            foreach (var item in newItems)
             {
-                Items.Clear();
-                foreach (var item in newItems)
-                {
-                    Items.Add(item);
-                }
+                Items.Add(item);
             }
 
-            SelectedValue = newItems[0].Id;
-            cvs.View.Refresh();
+            //SelectedValue = newItems[0].Id;
+            CollectionView.Refresh();
         }
 
         public ICommand ClearCommand { get; init; }
         public void Clear()
         {
             Items.Clear();
-            cvs.View.Refresh();
+            //CollectionView.Refresh();
         }
 
         static Person GenerateRandomPerson()
